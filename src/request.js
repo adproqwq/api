9885006d194b1ee7576551411629b0fabc6369d1
adproqwq <2757917031@qq.com>
2023-12-08 21:10:49 +0800

    fix: 请求添加User-Agent

--- src/request.js
+++ src/request.js
@@ -4,7 +4,14 @@ function getTextareaValue(){
 };
 
 function get(parameter){
-    $.get('api/dy.php?lj=' + parameter + '&return=json',function(data){
-        alert('解析完成！视频直链：' + data.url);
+    $.ajax({
+        type: 'GET',
+        url: 'api/dy.php?lj=' + parameter + '&return=json',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
+        },
+        success: function(data){
+            alert('解析完成！视频直链：' + data.url);
+        }
     });
 };
