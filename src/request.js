226a57661fcd1d587e86a0fced5b453a6b98b844
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-12-09 02:46:27 +0800

    搜索、下载功能正式生效 (#2)
    * feat: songList preview
    
    * feat: 搜索及下载功能
    
    * fix: JSON数组问题
    
    * fix: 歌手获取失败
    
    * fix: 多余引号
    
    * fix: 歌手首行显示错误
    
    * fix: 多个歌手只显示一个
    
    * fix: 监听事件不生效
    
    * fix: 修复表头与主体间的undefined问题
    
    * fix: 修复修复上一个问题带来的新的问题
    
    ---------
    
    Co-authored-by: adproqwq <adpro_qwq@outlook.com>

--- src/request.js
+++ src/request.js
@@ -1,14 +1,58 @@
 function getInput(){
     var parameter = document.getElementById('name').value;
-    get(parameter);
+    getSongList(parameter);
 };
 
-function get(parameter){
-    $.ajax({
-        type: 'GET',
-        url: '/oi/API/QQ_Music/?msg=' + parameter,
-        success: function(data){
-            alert(data.data[0].url);
+function getSongList(parameter){
+    axios.get('/oi/API/QQ_Music/?msg=' + parameter)
+    .then(function(data){
+            var songData = data.data.data;
+            initTable = `
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>封面图</th>
+                        <th>歌手</th>
+                        <th>专辑</th>
+                        <th>歌名</th>
+                        <th>操作</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>`;
+            document.getElementById('songList').innerHTML = initTable;
+            var eachSongDetails = '';
+            for(var i in songData){
+                var songSingers;
+                songSingers = '';
+                for(var j in songData[i].singers){
+                    songSingers += songData[i].singers[j] + '，';
+                }
+                eachSongDetails += `
+                <tr>
+                    <td><img src="${songData[i].picture}" height="100" weight="100"></td>
+                    <td>${songSingers}</td>
+                    <td>${songData[i].album}</td>
+                    <td>${songData[i].song}</td>
+                    <td><button name='download' index=${String(i)}>下载</button></td>
+                </tr>`;
+            };
+            var songList = document.querySelector('tbody');
+            songList.innerHTML = eachSongDetails;
+            songList.addEventListener('click',function(e){
+                if(e.target.getAttribute('name') == 'download'){
+                    var index = String(Number(e.target.getAttribute('index')) + 1);
+                    download(index);
+                }
+            });
         }
+    );
+};
+
+function download(index){
+    axios.get('/oi/API/QQ_Music/?msg=' + document.getElementById('name').value + '&n=' + index)
+    .then(function(data){
+        var songData = data.data.data;
+        window.open(songData.music);
     });
 };
