bef4aa91bbc45321832cbf63c7fa05545e50ed1a
adproqwq <2757917031@qq.com>
2023-12-09 01:06:36 +0800

    feat: songList preview

--- src/request.js
+++ src/request.js
@@ -7,6 +7,17 @@ function get(parameter){
     axios.get('/oi/API/QQ_Music/?msg=' + parameter)
     .then(function(data){
             console.log(data);
+            initTable = `
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>收藏</th>
+                        <th>操作</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>`;
+            document.getElementById('songList').innerHTML = initTable;
         }
     );
 };
