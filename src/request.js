14990f2d9e7ef5883e7156e31afacbb817821416
adproqwq <2757917031@qq.com>
2023-12-09 01:47:03 +0800

    feat: 搜索及下载功能

--- src/request.js
+++ src/request.js
@@ -1,23 +1,59 @@
 function getInput(){
     var parameter = document.getElementById('name').value;
-    get(parameter);
+    getSongList(parameter);
 };
 
-function get(parameter){
+function getSongList(parameter){
     axios.get('/oi/API/QQ_Music/?msg=' + parameter)
     .then(function(data){
-            console.log(data.data);
+            var songData = data.data.data;
+            console.log(songData);
             initTable = `
             <table border="1">
                 <thead>
                     <tr>
-                        <th>收藏</th>
+                        <th>封面图</th>
+                        <th>歌手</th>
+                        <th>专辑</th>
+                        <th>歌名</th>
                         <th>操作</th>
                     </tr>
                 </thead>
                 <tbody></tbody>
             </table>`;
             document.getElementById('songList').innerHTML = initTable;
+            for(var i = 0;i < songData.length;i++){
+                for(var j = 0;j < songData.singers.length;j++){
+                    var songSingers;
+                    songSingers += songData[i].singers[j] + '，';
+                }
+                var eachSongDetails;
+                eachSongDetails += `
+                <tr>
+                    <td><img src="${songData[i].picture}"></td>
+                    <td>${songSingers}"</td>
+                    <td>${songData[i].album}"</td>
+                    <td>${songData[i].song}></td>
+                    <td><button name='download' index=${String(i)}>下载</button></td>
+                </tr>`;
+            };
+            document.querySelector('tbody').innerHTML = eachSongDetails;
         }
     );
 };
+
+function download(index){
+    axios.get('/oi/API/QQ_Music/?msg=' + document.getElementById('name').value + '&n=' + index)
+    .then(function(data){
+        var songData = data.data.data;
+        window.open(songData.music);
+    });
+};
+
+var songList = document.querySelector('tbody');
+songList.addEventListener('click',function(e){
+    if(e.target.getAttribute('name') == 'download'){
+        var index = String(Number(e.target.getAttribute('index')) + 1);
+        download(index);
+    }
+});
