8e520391f450ee1369cfec05a431698c4559fa17
adproqwq <2757917031@qq.com>
2023-12-09 00:40:57 +0800

    test

--- src/request.js
+++ src/request.js
@@ -6,7 +6,7 @@ function getInput(){
 function get(parameter){
     axios('/oi/API/QQ_Music/?msg=' + parameter)
     .then(data => {
-            alert(data.data[0]);
+            alert(data.data[1]);
         }
     );
 };
