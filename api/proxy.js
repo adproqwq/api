ac453433a9af822b48d5d0b47a18459af1aafb05
adproqwq <2757917031@qq.com>
2023-12-08 21:37:16 +0800

    fix: 尝试修复跨域问题

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,21 @@
 // 该服务为 vercel serve跨域处理
 const {
     createProxyMiddleware
-    } = require('http-proxy-middleware')
-    module.exports = (req, res) => {
+} = require('http-proxy-middleware')
+module.exports = (req, res) => {
     let target = ''
     // 代理目标地址
     target = 'http://api.tangdouz.com'
     // 创建代理对象并转发请求
     createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-    // 通过路径重写，去除请求路径中的 `/api`
-    '^/api/': '/'
-    }
+        target,
+        changeOrigin: true,
+        pathRewrite: {
+            // 通过路径重写，去除请求路径中的 `/api`
+            '^/api/': '/'
+        },
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'
+        },
     })(req, res)
-    }
+}
