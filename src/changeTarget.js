de6513906f253e1e3651066642677008995cb22f
adproqwq <2757917031@qq.com>
2023-12-09 14:12:02 +0800

    fix: some problems

--- src/changeTarget.js
+++ src/changeTarget.js
@@ -2,6 +2,7 @@ document.getElementById("searchTarget").addEventListener("change",function(e){
     if(e.target.tagName == "SELECT"){
         var userselect = document.getElementById("searchTarget");
         var index = userselect.selectedIndex;
+        if(document.getElementById(userselect.options[index].value)) document.body.removeChild(document.getElementById(userselect.options[index].value));
         var newScript = document.createElement('script');
         newScript.id = userselect.options[index].value;
         newScript.src = './src/' + userselect.options[index].value + '.js';
