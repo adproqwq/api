4512f833c09d2cfbf70deac905853fed9c2fb351
adproqwq <2757917031@qq.com>
2023-12-09 14:27:58 +0800

    fix: netease无法下载

--- src/netease.js
+++ src/netease.js
@@ -51,7 +51,7 @@ function getSongList(parameter){
 
 function download(index){
     let downloadPage = window.open('','_blank');
-    axios.get('/oi/API/Music_163?msg=' + document.getElementById('name').value + '&n=' + index)
+    axios.get('/oi/API/Music_163?name=' + document.getElementById('name').value + '&n=' + index)
     .then(function(data){
         if(data.data.code == '0'){
             var songData = data.data.data;
