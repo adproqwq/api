217bf2c8adc56325508eb3b04198ffeaa15fa8ff
adproqwq <2757917031@qq.com>
2023-12-09 12:57:50 +0800

    fix: some problems

--- src/changeTarget.js
+++ src/changeTarget.js
@@ -2,7 +2,8 @@ document.getElementById("searchTarget").addEventListener("change",function(e){
     if(e.target.tagName == "SELECT"){
         var userselect = document.getElementById("searchTarget");
         var index = userselect.selectedIndex;
-        if(userselect.options[index].value == 'QQMusic') document.getElementById('import').src = './src/' + userselect.options[index].value + '.js';
-        else if(userselect.options[index].value == 'netease') document.getElementById('import').src = './src/' + userselect.options[index].value + '.js';
+        document.getElementById('import').src = './src/' + userselect.options[index].value + '.js';
+        document.body.appendChild(document.getElementById('import'));
+        document.body.appendChild('<script src="./src/changeTarget.js"></script>');
     };
 });
