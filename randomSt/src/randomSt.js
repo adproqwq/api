5392b4bfcba3a0b498c44812fb7f462f32cd2d90
adproqwq <2757917031@qq.com>
2023-12-16 14:38:08 +0800

    回滚

--- randomSt/src/randomSt.js
+++ randomSt/src/randomSt.js
@@ -1,6 +1,4 @@
 function getImage(){
-    showImage = document.createElement('img');
-    showImage.setAttribute('id','st');
-    showImage.setAttribute('src','https://se.csnmb.com/API/ql.php');
-    document.getElementById('imageDisplay').appendChild(showImage);
+    showImage = `<img src="https://se.csnmb.com/API/ql.php">`;
+    document.getElementById('imageDisplay').innerHTML = showImage;
 };
