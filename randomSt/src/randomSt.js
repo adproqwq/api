d23e6a7e2bec03011a7fc53d10f8a006f847c5a8
adproqwq <2757917031@qq.com>
2023-12-16 14:35:35 +0800

    fix: 无法刷新

--- randomSt/src/randomSt.js
+++ randomSt/src/randomSt.js
@@ -1,4 +1,6 @@
 function getImage(){
-    showImage = `<img src=https://se.csnmb.com/API/ql.php>`;
-    document.getElementById('imageDisplay').innerHTML = showImage;
+    showImage = document.createElement('img');
+    showImage.setAttribute('id','st');
+    showImage.setAttribute('src','https://se.csnmb.com/API/ql.php');
+    document.getElementById('imageDisplay').appendChild(showImage);
 };
