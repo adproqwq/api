441626738c587e498dde467d0bc6a4574edab503
adproqwq <2757917031@qq.com>
2023-12-29 20:06:05 +0800

    feat: 疯狂星期四

--- crazyThursday/src/crazyThursday.js
+++ crazyThursday/src/crazyThursday.js
@@ -1,5 +1,3 @@
-function get(){
-    axios.get('/adpro/ybapi/API/kfc.php').then(function(data){
-        document.getElementById('emojiDisplay').innerHTML = data;
-    });
-};
+axios.get('/adpro/ybapi/API/kfc.php').then(function(data){
+    document.getElementById('emojiDisplay').innerHTML = data;
+});
