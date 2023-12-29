8edc0d634216567c4fb3a293ba40e9a415822c8b
adproqwq <2757917031@qq.com>
2023-12-29 23:51:02 +0800

    chore: replace "\n" to "<br>"

--- crazyThursday/src/crazyThursday.js
+++ crazyThursday/src/crazyThursday.js
@@ -1,6 +1,6 @@
 axios.get('/adpro/ybapi/API/kfc.php').then(function(data){
     var result = data.data;
     result = result.slice(1,-1);
-    result.replace(/\\n/,'<br>');
+    result = result.replace(/\\n/,'<br>');
     document.getElementById('content').innerHTML = result;
 });
