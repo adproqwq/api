f9dc84aafe045a23aab376728f96cd06b521b7f8
adproqwq <2757917031@qq.com>
2023-12-30 11:31:43 +0800

    feat: sky光遇工具

---
+++ sky/src/sky.js
@@ -0,0 +1,4 @@
+function getResult(type){
+    showResult = `<img src=https://api.t1qq.com/api/sky/sc/sc${type}?key=VidPUR98QLcAmU0ENgz0YwYmNq>`;
+    document.getElementById('resultDisplay').innerHTML = showResult;
+};
