a745a5eadfb2f884b7e10ab376f2c103f08f01c6
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-01-30 19:46:22 +0800

    feat(AIGirlfriends): error message

--- AIGirlfriends/src/AIGirlfriends.js
+++ AIGirlfriends/src/AIGirlfriends.js
@@ -11,13 +11,14 @@ function getResponse(msg, personality){
         "jj": "婧枫",
         "mm": "沫沫"
     };
+    document.getElementById('chatArea').innerHTML += `你：${msg}<br>`;
     axios.get(`/adpro/lolimi/API/AI/${personality}.php?msg=${msg}`).then(function(data){
         if(data.data.code == '200'){
             let result = data.data.data;
-            document.getElementById('chatArea').innerHTML += `你：${msg}<br>`;
             setTimeout(function(){
                 document.getElementById('chatArea').innerHTML += `${name[personality]}：${result.output}<br>`;
             },1000);
         }
+        else document.getElementById('chatArea').innerHTML += `System：对方暂时离线<br>`;
     });
 };
