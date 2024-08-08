6491c71144e41bb9290c126358e6d1d391c4763b
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-08-08 22:33:01 +0800

    feat: 无结果处理

--- src/utils/music/fetch.ts
+++ src/utils/music/fetch.ts
@@ -1,18 +1,31 @@
 import renderer from './renderer';
 import type { IQQResultData, INetEaseResultData } from './types';
+import { snackbar } from 'mdui';
 
 const basicUrl = 'https://api.xingzhige.com';
 
 const QQ = async (target: string) => {
   const apiUrl = `${basicUrl}/API/QQmusicVIP/`;
   const responce: IQQResultData = await (await fetch(`${apiUrl}?max=20&name=${target}`)).json();
-  renderer(`${apiUrl}?name=${target}`, responce);
+  if(responce.code == -201){
+    snackbar({
+      message: '搜索无结果',
+      placement: 'top',
+    });
+  }
+  else renderer(`${apiUrl}?name=${target}`, responce);
 };
 
 const NetEase = async (target: string) => {
   const apiUrl = `${basicUrl}/API/NetEase_CloudMusic_new/`;
   const responce: INetEaseResultData = await (await fetch(`${apiUrl}?pagesize=20&name=${target}`)).json();
-  renderer(`${apiUrl}?name=${target}`, responce);
+  if(responce.code == -404){
+    snackbar({
+      message: '搜索无结果',
+      placement: 'top',
+    });
+  }
+  else renderer(`${apiUrl}?name=${target}`, responce);
 };
 
 export default async (platform: string, target: string) => {
