7486c5e263e9f28bd0c12fb0eda4adfde9c352ea
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-08-08 22:51:10 +0800

    feat: 允许调整主题色

--- src/components/MusicPage.vue
+++ src/components/MusicPage.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import search from '@/utils/music/search';
 import { defineComponent } from 'vue';
-import type { TextField } from 'mdui';
+import { type TextField, prompt, setColorScheme, removeColorScheme } from 'mdui';
 import '@/assets/music.css';
 import '@mdui/icons/search';
 
@@ -11,6 +11,37 @@ export default defineComponent({
       const target = (document.querySelector('#searchBox') as TextField).value;
       search(target);
     },
+    setColourTheme(){
+      prompt({
+        headline: '调整主题色',
+        description: '输入一个以#开头的16进制颜色值，留空恢复默认',
+        confirmText: '应用',
+        cancelText: '取消',
+        closeOnEsc: true,
+        closeOnOverlayClick: true,
+        textFieldOptions: {
+          autosize: true,
+          autocomplete: 'off',
+          autofocus: true,
+        },
+        onConfirm: (value) => {
+          if(value != ''){
+            localStorage.setItem('colourTheme', value);
+            setColorScheme(value);
+          }
+          else{
+            localStorage.removeItem('colourTheme');
+            removeColorScheme();
+          }
+        },
+      });
+    },
+  },
+  mounted(){
+    if(localStorage.getItem('colourTheme')){
+      const colourHex = localStorage.getItem('colourTheme')!;
+      setColorScheme(colourHex);
+    }
   },
 });
 </script>
@@ -21,6 +52,7 @@ export default defineComponent({
       <mdui-segmented-button value="QQ">小Q</mdui-segmented-button>
       <mdui-segmented-button value="NetEase">小云</mdui-segmented-button>
     </mdui-segmented-button-group>
+    <mdui-button-icon variant="standard" icon="palette--outlined" @click="setColourTheme()"></mdui-button-icon>
   </div>
   <div>
     <mdui-text-field id="searchBox" variant="filled" label="搜索" placeholder="请输入信息" clearable>
